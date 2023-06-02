cd3d1439acc3fcd41b1da3782615c05a49e3591a
Msdee12 <stevendeodatha@gmail.com>
2023-06-01 18:02:43 -0700

    change project title

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,7 +7,7 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
    {{-- @vite('resources/css/app.css') --}}
-  <title>Marketplace</title>
+  <title>show me where to buy it </title>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <script src="{{asset('assets/js/jquery-3.6.0.min.js')}}"></script>
   <link rel="stylesheet" href="{{asset('assets/css/tailwind.css')}}" />
