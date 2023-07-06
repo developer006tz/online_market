a99102afb31f7591fdc182a18f3b78c9eb3dbf18
developer006 <developer.ludovic@gmail.com>
2023-07-07 01:20:20 +0300

    add build

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,9 +33,9 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+            @if (Route::has('register'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __('Or Register here?') }}
                 </a>
             @endif
 
