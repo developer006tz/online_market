b09ad57098e3ab130dbffa82da94f836e41a2c36
developer006 <developer.ludovic@gmail.com>
2023-07-07 00:44:03 +0300

    add carousel

--- resources/views/website/app.blade.php
+++ resources/views/website/app.blade.php
@@ -8,8 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
    {{-- @vite('resources/css/app.css') --}}
   <title>show me where to buy it </title>
-  <script src="{{asset('assets/js/jquery-3.6.0.min.js')}}"></script>
-  <link rel="stylesheet" href="{{asset('assets/css/tailwind.css')}}" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>  <link rel="stylesheet" href="{{asset('assets/css/tailwind.css')}}" />
   {{-- <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.6/flowbite.min.css"  rel="stylesheet" /> --}}
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
     integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
